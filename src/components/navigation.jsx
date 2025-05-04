322a6d7cae413354d2c994c1a1e328a2eb824986
Blessdbnjmn <113176355+Blessdcode@users.noreply.github.com>
2025-05-04 03:36:55 +0100

    nav done

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -7,46 +7,46 @@ const Navigation = () => {
     const [isOpen, setIsOpen] = useState(false);
   return (
     // <header>
-      <div className="header-area">
-        {/* <!-- Navigation --> */}
-        <nav>
-          <div className="logo">
-            <Link to="/">
-              <img src={logo} alt="Logo" />
+    <div className="header-area">
+      {/* <!-- Navigation --> */}
+      <nav>
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="Logo" />
+          </Link>
+        </div>
+        <ul id="Navigation" className={isOpen ? "" : "activeMenu"}>
+          <li>
+            <Link to="/" className="active">
+              Home
             </Link>
-          </div>
-          <ul id="Navigation">
-            <li>
-              <Link to="/" className="active">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/causes">Causes</Link>
-            </li>
-            <li>
-              <Link to="/impact">Impact</Link>
-            </li>
-            <li>
-              <Link to="/blog">Blog</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-          <div className="donate_now">
-            <Link to="donate" className="btn">
-              Donate Now
-            </Link>
-          </div>
-          <div id="menuIcon">
-            <AiOutlineMenu size={28} />
-          </div>
-        </nav>
-      </div>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/causes">Causes</Link>
+          </li>
+          <li>
+            <Link to="/impact">Impact</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <div className="donate_now">
+          <Link to="donate" className="btn">
+            Donate Now
+          </Link>
+        </div>
+        <div id="menuIcon" onClick={() => setIsOpen(!isOpen)}>
+          <AiOutlineMenu size={28} />
+        </div>
+      </nav>
+    </div>
     // </header>
   );
 };
